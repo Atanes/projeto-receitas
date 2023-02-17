908cea503800796417e4615df9c81629cae03a16
Atanes <aatanes100@gmail.com>
2023-02-17 17:05:55 -0300

    Conexão com o banco de dados, salvando e recuperando dados

--- src/main/java/com/iridimit/receitaapi/controller/ReceitaController.java
+++ src/main/java/com/iridimit/receitaapi/controller/ReceitaController.java
@@ -1,12 +1,10 @@
 package com.iridimit.receitaapi.controller;
 
 import com.iridimit.receitaapi.model.Receita;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.iridimit.receitaapi.repository.ReceitaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -14,20 +12,17 @@ import java.util.List;
 @RequestMapping(value = "/receitas")
 public class ReceitaController {
 
+    @Autowired
+    ReceitaRepository receitaRepository;
+
     @GetMapping
     public List<Receita> listarReceitas(){
-        Receita receita1 = new Receita(1L, new Date(), "Bolo de fubá");
-        Receita receita2 = new Receita(2L, new Date(), "Feijão de Corda");
-        receita1.setAutor("Atanes");
-        receita2.setAutor("Atanes");
-        List<Receita> listagemGeral = new ArrayList<>();
-        listagemGeral.add(receita1);
-        listagemGeral.add(receita2);
-        return listagemGeral;
+        return receitaRepository.findAll();
     }
 
-    @GetMapping(value = "/uma-receita")
-    public ResponseEntity<String> getReceita(){
-        return ResponseEntity.ok("Lista uma unica receita!!!!!!");
+    @PostMapping
+    public Receita salvarReceita(@RequestBody Receita receita){
+        receita.setDataInclusao(new Date());
+        return receitaRepository.save(receita);
     }
 }
--- src/main/java/com/iridimit/receitaapi/model/Receita.java
+++ src/main/java/com/iridimit/receitaapi/model/Receita.java
@@ -1,11 +1,13 @@
 package com.iridimit.receitaapi.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
 
@@ -13,19 +15,24 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Receita {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private Long id;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String nome;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String descricao;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private Date dataInclusao;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String autor;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String modoDePreparo;
+    @Transient
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private List<Ingrediente> ingredientes;
 
