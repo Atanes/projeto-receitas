aa8487e8355ab3d92856500522a13d58dbdf4745
Atanes <aatanes100@gmail.com>
2023-03-03 18:49:40 -0300

    Inclusão do campo data nos inserts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO receita (autor, descricao, modo_de_preparo, nome) VALUES ('Marcia Maria', 'Melhor Bolo de Fubá do Brasil', lo_from_bytea(0,'Separar todos os ingredientes....'), 'Bolo de Fubá');
-INSERT INTO receita (autor, descricao, modo_de_preparo, nome) VALUES ('Daniela', 'Arroz de forno Paulista', lo_from_bytea(0,'Separar todos os ingredientes....'), 'Arroz de forno');
-INSERT INTO receita (autor, descricao, modo_de_preparo, nome) VALUES ('Adriano Dantas', 'Pudim de leite com calda de caramelo', lo_from_bytea(0,'Separar todos os ingredientes....'), 'Pudim de leite');
+INSERT INTO public.receita (autor, data_inclusao, descricao, modo_de_preparo, nome) VALUES ('Marcia Maria', '2023-03-03', 'Melhor Bolo de Fubá do Brasil', lo_from_bytea(0,'Separar todos os ingredientes....'), 'Bolo de Fubá');
+INSERT INTO public.receita (autor, data_inclusao, descricao, modo_de_preparo, nome) VALUES ('Daniela', '2023-03-03', 'Arroz de forno Paulista', lo_from_bytea(0,'Separar todos os ingredientes....'), 'Arroz de forno');
+INSERT INTO public.receita (autor, data_inclusao, descricao, modo_de_preparo, nome) VALUES ('Adriano Dantas', '2023-03-03', 'Pudim de leite com calda de caramelo', lo_from_bytea(0,'Separar todos os ingredientes....'), 'Pudim de leite');
